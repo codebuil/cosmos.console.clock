126cdff38a494e9780c379b90d96b2c1c03dd5b7
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-12-30 20:59:44 +0000

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Sys = Cosmos.System;
+using System.Threading;
+namespace clocks
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("time");
+        }
+
+        protected override void Run()
+        {
+            DateTime dt = DateTime.Now;
+
+            Console.SetCursorPosition(1, 10);
+            Console.WriteLine(dt.ToString()+"                         ");
+            
+            Thread.Sleep(1000);
+
+
+        }
+    }
+}
